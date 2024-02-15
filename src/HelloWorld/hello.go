81bbe9b3e389f868d6ec7414ea3e1ff0a3addc80
Lucas Vinicius <lvinicius@kornerstonecredit.com>
2024-02-15 19:46:47 -0300

    doing some basic exercises in golang to memorize the syntax

---
+++ src/HelloWorld/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	print("Hello, world!\n")
+	fmt.Println("Here is my address: Rua 147, Patos de Minas - MG, Brasil")
+}
+
+// author: Lucas Vinicius
---
+++ src/Strings/strings.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstString = "this is a string"
+	fmt.Println(firstString)
+	secondString := "and this one, is my second string"
+	fmt.Print(secondString)
+}
